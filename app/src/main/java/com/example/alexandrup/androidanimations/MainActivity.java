13b0b8dbb4e4bccd5f46bf6e378d7b0cfe89f70e
PerryB52 <alex2pereteanu@gmail.com>
2017-01-18 23:08:31 +0200

    Commit 2 - Animation Listener

--- app/src/main/java/com/example/alexandrup/androidanimations/MainActivity.java
+++ app/src/main/java/com/example/alexandrup/androidanimations/MainActivity.java
@@ -6,8 +6,9 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Animation.AnimationListener {
     private ImageView mImageView;
 
     private Animation mRotateAnim;
@@ -27,27 +28,46 @@ public class MainActivity extends AppCompatActivity {
     public void scaleAnimation(View view) {
 
         mScaleAnim = AnimationUtils.loadAnimation(this, R.anim.scale_anim);
+        mScaleAnim.setAnimationListener(this);
         mImageView.startAnimation(mScaleAnim);
     }
 
     public void translateAnimation(View view) {
 
         mTranslateAnim = AnimationUtils.loadAnimation(this, R.anim.translate_anim);
+        mTranslateAnim.setAnimationListener(this);
         mImageView.startAnimation(mTranslateAnim);
     }
 
     public void alphaAnimation(View view) {
         mAlphaAnim = AnimationUtils.loadAnimation(this, R.anim.alpha_anim);
+        mAlphaAnim.setAnimationListener(this);
         mImageView.startAnimation(mAlphaAnim);
     }
 
     public void rotateAnimation(View view) {
 
         mRotateAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
+        mRotateAnim.setAnimationListener(this);
         mImageView.startAnimation(mRotateAnim);
     }
 
 
+    @Override
+    public void onAnimationStart(Animation animation) {
 
+        Toast.makeText(MainActivity.this, "Animation Started", Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+        Toast.makeText(MainActivity.this, "Animation Ended", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+        Toast.makeText(MainActivity.this, "Animation Repeated", Toast.LENGTH_SHORT).show();
+    }
 }
